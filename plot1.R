cb9f6e9c18d1ea188b4026b8e750ea091f982328
christinch3n <c.jandrasits@gmail.com>
2014-10-12 14:31:19 +0200

    Added script and png for first plot.

---
+++ plot1.R
@@ -0,0 +1,3 @@
+data <- read.table("C:/Users/Christine/Documents/git-repositories/courseradatascience/ExpData_project1/household_power_consumption.txt", sep=";", header=T, 
+                   stringsAsFactors=F, na.strings="?")
+
